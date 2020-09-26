bd58320d74dede2ee57c064b93edc0d9386f7e32
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-09-26 12:57:31 -0300

    Created Http Response static class

---
+++ server/src/resources/http/http-response.ts
@@ -0,0 +1,41 @@
+import { HttpResponseData } from '../../protocols/infra'
+
+export class HttpResponse {
+  static ok(body: object | object[]) {
+    return <HttpResponseData> {
+      status: 200,
+      body
+    }
+  }
+
+  static badRequest(error: Error | string) {
+    return <HttpResponseData> {
+      status: 400,
+      body: {
+        error: getErrorMessage(error)
+      }
+    }
+  }
+
+  static notFound(error: Error | string) {
+    return <HttpResponseData> {
+      status: 404,
+      body: {
+        error: getErrorMessage(error)
+      }
+    }
+  }
+
+  static serverError(error: Error | string) {
+    return <HttpResponseData> {
+      status: 500,
+      body: {
+        error: getErrorMessage(error)
+      }
+    }
+  }
+}
+
+function getErrorMessage(error: Error | string) {
+  return error instanceof Error ? error.message : error
+}
