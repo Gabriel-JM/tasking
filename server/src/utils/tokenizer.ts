621d22c510e4a743a93faf17e9fea3a7253f1fd4
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-02 21:42:12 -0300

    Create Tokenizer to implements Token Generator interface

---
+++ server/src/utils/tokenizer.ts
@@ -0,0 +1,22 @@
+import { User } from '../protocols/models'
+import { TokenGenerator } from '../protocols/utils'
+import JWT from 'jsonwebtoken'
+
+const maxExperitionDate = 24 * 60 * 60
+
+export class Tokenizer implements TokenGenerator {
+
+  constructor(private readonly secretKey: string) {}
+
+  generate({ id, username }: User) {
+    return JWT.sign({ id, username }, this.secretKey, {
+      expiresIn: maxExperitionDate
+    })
+  }
+
+  verify(tokenToVerify: string) {
+    const verification = JWT.verify(tokenToVerify, this.secretKey)
+
+    return verification.toString()
+  }
+}
