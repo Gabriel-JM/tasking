97d216a780b68a134e5073d51828d6c2a14536aa
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-07 18:01:21 -0300

    Created a sqlite error responser

---
+++ server/src/resources/errors/response-by-sqlite-error.ts
@@ -0,0 +1,14 @@
+import { StringKeyAccess } from '../../protocols/utils'
+import { HttpResponse } from '../http/http-response'
+
+export const responseBySqliteError: StringKeyAccess = {
+  SQLITE_CONSTRAINT(msg: string) {
+    if(msg.includes('UNIQUE')) {
+      return HttpResponse.notAcceptable(msg)
+    }
+  },
+
+  SQLITE_ERROR(msg: string) {
+    return HttpResponse.serverError(msg)
+  }
+}
