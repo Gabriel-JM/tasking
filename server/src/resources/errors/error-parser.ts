0e7aa442aacded874670602afc74f31ff3f88891
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-07 20:42:18 -0300

    Added return type for catch method of ErrorParser

--- server/src/resources/errors/error-parser.ts
+++ server/src/resources/errors/error-parser.ts
@@ -1,9 +1,10 @@
+import { HttpResponseData } from '../../protocols/infra'
 import { SqliteError } from '../../protocols/utils'
 import { HttpResponse } from '../http/http-response'
 import { responseBySqliteError } from './response-by-sqlite-error'
 
 export class ErrorParser {
-  static catch(err: Error | SqliteError) {
+  static catch(err: Error | SqliteError): HttpResponseData {
     if('code' in err) {
       const { code, message } = err as SqliteError
       return responseBySqliteError[code](message)
