5dd54b349e6ded61a80dd05e79c6402f972f4495
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-09 22:07:54 -0300

    Created Title component

---
+++ web/src/components/Title/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './title.css'
+
+function Title() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <h1>Tasking</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Title 
