73986da0cd0e9903f14571ef65fef9930976ce86
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-13 01:29:03 -0300

    Show newly created components on screen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import InputField from './components/Forms/InputField'
 import Title from './components/Title'
 
 function App() {
-  return <Title />
+  return (
+    <>
+      <Title />
+      <InputField label="Texto" type="text" />
+      <InputField label="Texto" type="text" required errors />
+    </>
+  )
 }
 
 export default App
