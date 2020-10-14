2825a0c5ce8924042e0832c450453a5a1591abf9
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-14 14:04:24 -0300

    Adding components tests in App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,13 +2,26 @@ import React from 'react'
 import InputField from './components/Forms/InputField'
 import Title from './components/Title'
 
+import './assets/css/buttons.css'
+
 function App() {
   return (
-    <>
+    <div style={{ margin: '6px' }}>
       <Title />
       <InputField label="Texto" type="text" />
-      <InputField label="Texto" type="text" required errors />
-    </>
+      {/* <InputField label="Texto" type="text" required errors={{
+        type: 'required',
+        message: 'Required fields'
+      }}/>
+      <InputField label="Texto" type="text" errors={{
+        type: 'field',
+        message: 'Required fields, of many orther inputs, of thounsand of orther things about'
+      }} /> */}
+      <button className="btn">Botão</button>
+      <br/>
+      <br/>
+      <button className="btn primary">Botão</button>
+    </div>
   )
 }
 
