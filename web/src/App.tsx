cb6fa444ff07e296222ef5ec04260ced2e8b3188
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-14 21:05:13 -0300

    Change App.tsx to use AppRouter

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,39 +1,18 @@
 import React from 'react'
-import InputField from './components/Forms/InputField'
+import { BrowserRouter } from 'react-router-dom'
 import Title from './components/Title'
+import AppRouter from './router'
 
 import './assets/css/buttons.css'
 
 function App() {
   return (
-    <div style={{ margin: '6px' }}>
+    <>
       <Title />
-      <InputField label="Texto" type="text" />
-      <InputField label="Texto" type="text" required errors={{
-        type: 'required',
-        message: 'Required fields'
-      }}/>
-      <InputField label="Texto" type="text" errors={{
-        type: 'field',
-        message: 'Required fields, of many orther inputs, of thounsand of orther things about'
-      }} /> 
-      <button className="btn">Botão normal</button>
-      <br/>
-      <br/>
-      <button className="btn" disabled>Botão normal desabiletado</button>
-      <br/>
-      <br/>
-      <button className="btn primary">Botão</button>
-      <br/>
-      <br/>
-      <button className="btn primary" disabled>Botão</button>
-      <br/>
-      <br/>
-      <button className="btn danger">Botão</button>
-      <br/>
-      <br/>
-      <button className="btn danger" disabled>Botão</button>
-    </div>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </>
   )
 }
 
