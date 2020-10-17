9eaf3255c08ecf5d06700634ec79bac82f5f69b2
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-17 00:00:59 -0300

    Add the initial components to the login page

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import InputField from '../../components/Forms/InputField'
+import { useForm } from 'react-hook-form'
+import './login.css'
+
+interface LoginData {
+  username: string
+  password: string
+}
 
 function Login() {
+  const { register, handleSubmit, errors } = useForm<LoginData>()
+
+  function onFormSubmit(data: LoginData) {
+    console.log('data', data)
+  }
+
   return (
-    <div>Login</div>
+    <section className="login-container">
+      <form className="login-form" onSubmit={handleSubmit(onFormSubmit)}>
+        <InputField
+          label="Username"
+          required
+          name="username"
+          placeholder="example.name"
+          ref={register}
+        />
+        <InputField
+          label="Password"
+          type="password"
+          required
+          name="password"
+          placeholder="secret_password"
+          ref={register}
+        />
+        <button className="btn primary">Login</button>
+      </form>
+    </section>
   )
 }
 
