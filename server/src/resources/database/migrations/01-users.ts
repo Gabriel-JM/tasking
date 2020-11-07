1f57638cf4bcaca559f17fd0e74524a4ecda804a
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-07 14:35:13 -0300

    Updating users migration to use name and email

--- server/src/resources/database/migrations/01-users.ts
+++ server/src/resources/database/migrations/01-users.ts
@@ -3,7 +3,9 @@ import Knex from 'knex'
 export function up(knex: Knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary().notNullable()
-    table.string('username', 120).unique().notNullable()
+    table.string('name', 120).notNullable()
+    table.string('email', 150).notNullable()
+    table.string('username', 80).unique().notNullable()
     table.string('password').notNullable()
   })
 }
