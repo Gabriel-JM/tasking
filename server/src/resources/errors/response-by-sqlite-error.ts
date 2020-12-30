8c2be2dae86f63c4b1c6dd4dba2f92f4488e0fe7
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-12-30 16:59:40 -0300

    Correct error message parameters

--- server/src/resources/errors/response-by-sqlite-error.ts
+++ server/src/resources/errors/response-by-sqlite-error.ts
@@ -4,13 +4,19 @@ import { HttpResponse } from '../http/http-response'
 export const responseBySqliteError: StringKeyAccess = {
   SQLITE_CONSTRAINT(msg: string) {
     if(msg.includes('UNIQUE')) {
-      return HttpResponse.notAcceptable(msg)
+      return HttpResponse.notAcceptable({
+        field: '',
+        error: msg
+      })
     }
 
     return this.SQLITE_ERROR(msg)
   },
 
   SQLITE_ERROR(msg: string) {
-    return HttpResponse.serverError(msg)
+    return HttpResponse.serverError({
+      field: '',
+      error: msg
+    })
   }
 }
