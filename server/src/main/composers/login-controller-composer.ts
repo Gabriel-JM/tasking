c80fa1febeec809dbc62237f681b19777aa8cdd7
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-12-30 17:00:55 -0300

    Correct LoginController instantiation

--- server/src/main/composers/login-controller-composer.ts
+++ server/src/main/composers/login-controller-composer.ts
@@ -2,12 +2,14 @@ import { LoginController } from '../../controllers/login/login-controller'
 import { EnvironmentOptions } from '../../protocols/infra'
 import { LoginRepository } from '../../repositories/login/login-repository'
 import { connectionDatabase } from '../../resources/database/connection'
+import { EmailValidator } from '../../utils/helpers/email-validator'
 import { TextHasher } from '../../utils/helpers/hasher/text-hasher'
 import { Tokenizer } from '../../utils/helpers/token/tokenizer'
 
 export default () => {
   const passwordHasher = new TextHasher()
   const tokenGenerator = new Tokenizer(process.env.SECRET_KEY as string)
+  const emailValidator = new EmailValidator()
 
   const knexDB = connectionDatabase(process.env.NODE_ENV as EnvironmentOptions)
   const loginRepository = new LoginRepository('users', knexDB)
@@ -15,7 +17,8 @@ export default () => {
   const loginController = new LoginController(
     loginRepository,
     passwordHasher,
-    tokenGenerator
+    tokenGenerator,
+    emailValidator
   )
 
   return loginController
