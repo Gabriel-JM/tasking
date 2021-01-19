680415ec9668b13b5c8d2902357eeb29441413f9
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-19 16:35:41 -0300

    Adding CORS configurations

--- server/src/main/config/setup.ts
+++ server/src/main/config/setup.ts
@@ -1,8 +1,10 @@
 import express, { Application } from 'express'
 import dotenv from 'dotenv-safe'
+import { cors } from './cors'
 
 export default (app: Application) => {
   dotenv.config()
 
   app.use(express.json())
+  app.use(cors)
 }
