d5419202695bcdaea0deb07cf581834614090e8f
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-25 16:49:53 -0300

    Changing some protocols

--- server/src/protocols/domain/repositories/login-repository.ts
+++ server/src/protocols/domain/repositories/login-repository.ts
@@ -2,5 +2,6 @@ import { Repository } from '../../infra'
 import { User } from '../../models'
 
 export interface ILoginRepository extends Repository<User> {
+  find(id: number): Promise<User | null>
   findByUsernameAndPassword(content: { username: string, password: string }): Promise<User | null>
 }
--- server/src/protocols/infra/repository/repository.ts
+++ server/src/protocols/infra/repository/repository.ts
@@ -2,7 +2,7 @@ import { RepositoryDeleteMessage } from './repository-delete-message'
 
 export interface Repository<TEntry, TReturn = TEntry> {
   findAll?(): Promise<TReturn[]>
-  find?(id: number): Promise<TReturn>
+  find?(id: number): Promise<TReturn | null>
   save?(content: TEntry): Promise<TReturn>
   update?(content: TEntry): Promise<TReturn>
   delete?(id: number): Promise<RepositoryDeleteMessage>
