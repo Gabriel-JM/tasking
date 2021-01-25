4708f4bb882f4c9b29fc6fb50390901160c4bbab
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-25 20:47:38 -0300

    Adjusting login routes

--- server/src/main/routes/login/login-routes.test.ts
+++ server/src/main/routes/login/login-routes.test.ts
@@ -14,38 +14,59 @@ describe('Login Routes', () => {
     return await testDb.destroy()
   })
 
-  it('should return a 200 response with user credentials', async () => {
-    const newUser = {
-      username: 'any_user',
-      name: 'user name',
-      email: 'any@email.com',
-      password: 'any_password'
-    }
-
-    await testDb('users').insert(newUser)
-
-    const encryptedCredentials = Buffer
-      .from(`${newUser.username}:${newUser.password}`)
-      .toString('base64')
-    ;
-
-    const response = await request(app)
-      .get('/login')
-      .set('authorization', `Basic ${encryptedCredentials}`)
-    ;
-
-    expect(response.status).toBe(200)
-    expect(response.body).toBeDefined()
-    expect(response.body).toHaveProperty('token')
-  })
-
-  it('should return a 406 response, if request don\'t have authorization field', async () => {
-    const response = await request(app).get('/login')
-
-    expect(response.status).toBe(406)
-    expect(response.body).toEqual({
-      field: 'authorization',
-      error: 'Authorization field cannot be empty'
+  describe('/login', () => {
+    it('should return a 200 response with user credentials', async () => {
+      const newUser = {
+        username: 'any_user',
+        name: 'user name',
+        email: 'any@email.com',
+        password: 'any_password'
+      }
+  
+      await testDb('users').insert(newUser)
+  
+      const encryptedCredentials = Buffer
+        .from(`${newUser.username}:${newUser.password}`)
+        .toString('base64')
+      ;
+  
+      const response = await request(app)
+        .get('/login')
+        .set('authorization', 'Basic '+ encryptedCredentials)
+      ;
+  
+      expect(response.status).toBe(200)
+      expect(response.body).toBeDefined()
+      expect(response.body).toHaveProperty('token')
+    })
+  
+    it('should return a 406 response, if request don\'t have authorization field', async () => {
+      const response = await request(app).get('/login')
+  
+      expect(response.status).toBe(406)
+      expect(response.body).toEqual({
+        field: 'authorization',
+        error: 'Authorization field cannot be empty'
+      })
+    })
+  
+    it('should return a 404 response, if no user was found with the given credentials', async () => {
+      const encryptedCredentials = Buffer
+        .from('user:password')
+        .toString('base64')
+      ;
+  
+      const response = await request(app)
+        .get('/login')
+        .set('authorization', 'Basic '+ encryptedCredentials)
+      ;
+  
+      expect(response.status).toBe(404)
+      expect(response.body).toEqual({
+        field: '',
+        error: 'Invalid username or password'
+      })
     })
   })
+
 })
