d52b4e83281f1e2d09a91181916acebc7d2f10ab
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-04 13:25:24 -0300

    Adding the auth provider wrap

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Title from './components/Title'
 import AppRouter from './router'
+import { AuthProvider } from './providers/auth'
 
 import './assets/css/buttons.css'
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Title />
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
-    </>
+    </AuthProvider>
   )
 }
 
