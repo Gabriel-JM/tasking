f0a3023769244f74159e53decee78670fa774aab
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-22 18:19:27 -0300

    Correct middleware response function return value

--- server/src/protocols/infra/middleware/middleware-response-function.ts
+++ server/src/protocols/infra/middleware/middleware-response-function.ts
@@ -1,4 +1,4 @@
 import { MiddlewareHttpRequest } from '../http/middleware-http-request'
 import { MiddlewareResponse } from './middleware-response'
 
-export type MiddlewareResponseFunction = (request: MiddlewareHttpRequest) => Promise<MiddlewareResponse>
+export type MiddlewareResponseFunction = (request: MiddlewareHttpRequest) => MiddlewareResponse | Promise<MiddlewareResponse>
--- server/src/protocols/infra/middleware/middleware.ts
+++ server/src/protocols/infra/middleware/middleware.ts
@@ -1,5 +1,5 @@
 import { MiddlewareResponseFunction } from './middleware-response-function'
 
 export interface Middleware {
-  [key: string]: MiddlewareResponseFunction
+  [key: string]: MiddlewareResponseFunction | any
 }
