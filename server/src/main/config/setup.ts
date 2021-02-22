5ff4f62c591e6a0703d764c914bb7f24ff4a04a6
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-22 18:21:37 -0300

    Adding user login verification middleware

--- server/src/main/config/setup.ts
+++ server/src/main/config/setup.ts
@@ -1,5 +1,10 @@
 import express, { Application } from 'express'
 import cors from 'cors'
+import userVerificationMiddlewareComposer from '../composers/user-verification-middleware-composer'
+import { ExpressMiddlewareAdapter } from '../adapters/express-middleware-adapter'
+
+const userVerification = userVerificationMiddlewareComposer()
+const adapt = ExpressMiddlewareAdapter(userVerification)
 
 export default (app: Application) => {
   app.use(express.json())
@@ -7,4 +12,5 @@ export default (app: Application) => {
     maxAge: 86400,
     origin: process.env.WEB_URL
   }))
+  app.use(adapt(userVerification.verify))
 }
